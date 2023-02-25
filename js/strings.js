bd07c66ec4873b515bcbd1f532db96e2ed34ba08
Noel Kim <crlotwhite@gmail.com>
2023-02-25 22:42:17 +0800

    Add js codes
    localization, web-component, charts

---
+++ js/strings.js
@@ -0,0 +1,14 @@
+const en = {
+    headerText: "In December 2022, 96 respondents from various communities around the world shared their views on Vocaloid fandom tendencies, software experience, and artificial intelligence technology.",
+
+}
+
+const jp = {
+    headerText: "In December 2022, 96 respondents from various communities around the world shared their views on Vocaloid fandom tendencies, software experience, and artificial intelligence technology.",
+}
+
+const kr = {
+    headerText: "2022년 12월, 전세계 커뮤니티로부터 96 응답자가 보컬로이드 팬덤에서의 자신 및 소프트웨어 사용 경험, 그리고 인공지능 기술에 대한 의견을 공유해주었습니다.",
+}
+
+let currentLanguage = en;
