63de51ee3bd320e358fe49d1fb2225e3f9142d6a
Noel Kim <crlotwhite@gmail.com>
2023-03-02 23:17:03 +0800

    Update chart.js
    make the code simple

--- js/charts.js
+++ js/charts.js
@@ -1,21 +1,32 @@
-function chart1(canvasId, lang) {
+const pieColors = [
+    'rgb(255, 99, 132)',
+    'rgb(54, 162, 235)',
+    'rgb(255, 205, 86)',
+    'rgb(255, 205, 255)',
+    'rgb(157,241,169)',
+];
+
+function barColors(num) {
+    let colors = [];
+    for (let i=0;i<num;i++) {
+        colors.push('rgb(153, 102, 255)');
+    }
+    return colors;
+}
+
+function pieChart(canvasId, chartStrings, data) {
     new Chart(
         document.getElementById(canvasId).getContext('2d'),
         {
             type: "pie",
             data: {
-                labels: strings[lang].chart[canvasId].labels,
+                labels: chartStrings.labels,
                 datasets: [{
-                    data: [37, 38, 16, 5],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)',
-                        'rgb(255, 205, 86)',
-                        'rgb(255, 205, 255)'
-                    ],
+                    data: data,
+                    backgroundColor: pieColors,
                     hoverOffset: 4
                 }]
-    
+
             },
             options: {
                 responsive: true,
@@ -25,7 +36,7 @@ function chart1(canvasId, lang) {
                     },
                     title: {
                         display: true,
-                        text: strings[lang].chart[canvasId].question,
+                        text: chartStrings.question,
                         font: {
                             size: 18,
                         }
@@ -33,343 +44,78 @@ function chart1(canvasId, lang) {
                 }
             },
         }
-    )
+    );
+}
+
+function barChart(canvasId, chartStrings, data) {
+    new Chart(
+        document.getElementById(canvasId).getContext('2d'),
+        {
+            type: 'bar',
+            data: {
+                labels: chartStrings.labels,
+                datasets: [{
+                    label: chartStrings.label,
+                    data: data,
+                    backgroundColor: barColors(data.length)
+                }]
+            },
+            options: {
+                indexAxis: 'y',
+                elements: {
+                    bar: {
+                        borderWidth: 1,
+                    }
+                },
+                responsive: true,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: chartStrings.question,
+                        font: {
+                            size: 18,
+                        }
+                    }
+                }
+            },
+        }
+    );
+}
+
+
+function chart1(canvasId, lang) {
+    pieChart(canvasId, strings[lang].chart[canvasId], [37, 38, 16, 5]);
 }
 
 function chart2(canvasId, lang) {
-    new Chart(
-        document.getElementById(canvasId).getContext('2d'), 
-        {
-            type: 'bar',
-            data: {
-                labels: strings[lang].chart[canvasId].labels,
-                datasets: [{
-                    label: strings[lang].chart[canvasId].label,
-                    data: [
-                        75,
-                        29,
-                        52,
-                        51,
-                        2,
-                        4,
-                        4,
-                        16,
-                        3,
-                        5
-                    ],
-                    backgroundColor: [
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                    ]
-                }]
-            },
-            options: {
-                indexAxis: 'y',
-                elements: {
-                    bar: {
-                        borderWidth: 1,
-                    }
-                },
-                responsive: true,
-                plugins: {
-                    title: {
-                        display: true,
-                        text: strings[lang].chart[canvasId].question,
-                        font: {
-                            size: 18,
-                        }
-                    }
-                }
-            },
-        }
-    );
+    barChart(canvasId, strings[lang].chart[canvasId], [75, 29, 52, 51, 2, 4, 4, 16, 3, 5]);
 }
 
 function chart3(canvasId, lang) {
-    new Chart(
-        document.getElementById(canvasId).getContext('2d'),
-        {
-            type: "pie",
-            data: {
-                labels: strings[lang].chart[canvasId].labels,
-                datasets: [{
-                    data: [35, 36, 9, 6, 5, 5],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)',
-                        'rgb(255, 205, 86)',
-                        'rgb(255, 205, 255)',
-                        'rgb(157,241,169)',
-                        'rgb(250,119,45)',
-                    ],
-                    hoverOffset: 4
-                }]
-
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'bottom',
-                    },
-                    title: {
-                        display: true,
-                        text: strings[lang].chart[canvasId].question,
-                        font: {
-                            size: 18,
-                        }
-                    }
-                }
-            },
-        }
-    )
+    pieChart(canvasId, strings[lang].chart[canvasId], [35, 36, 9, 6, 5, 5]);
 }
 
 function chart4(canvasId, lang) {
-    new Chart(
-        document.getElementById(canvasId).getContext('2d'),
-        {
-            type: 'bar',
-            data: {
-                labels: strings[lang].chart[canvasId].labels,
-                datasets: [{
-                    label: strings[lang].chart[canvasId].label,
-                    data: [
-                        87,
-                        36,
-                        18,
-                        2,
-                        10,
-                        17,
-                        1,
-                    ],
-                    backgroundColor: [
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                        'rgb(153, 102, 255)',
-                    ]
-                }]
-            },
-            options: {
-                indexAxis: 'y',
-                elements: {
-                    bar: {
-                        borderWidth: 1,
-                    }
-                },
-                responsive: true,
-                plugins: {
-                    title: {
-                        display: true,
-                        text: strings[lang].chart[canvasId].question,
-                        font: {
-                            size: 18,
-                        }
-                    }
-                }
-            },
-        }
-    );
+    barChart(canvasId, strings[lang].chart[canvasId], [87, 36, 18, 2, 10, 17, 1]);
 }
 
 function chart5(canvasId, lang) {
-    new Chart(
-        document.getElementById(canvasId).getContext('2d'),
-        {
-            type: "pie",
-            data: {
-                labels: strings[lang].chart[canvasId].labels,
-                datasets: [{
-                    data: [5, 22, 18, 24, 27],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)',
-                        'rgb(255, 205, 86)',
-                        'rgb(255, 205, 255)',
-                        'rgb(157,241,169)',
-                    ],
-                    hoverOffset: 4
-                }]
-
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'bottom',
-                    },
-                    title: {
-                        display: true,
-                        text: strings[lang].chart[canvasId].question,
-                        font: {
-                            size: 18,
-                        }
-                    }
-                }
-            },
-        }
-    )
+    pieChart(canvasId, strings[lang].chart[canvasId], [5, 22, 18, 24, 27]);
 }
 
 function chart6(canvasId, lang) {
-    new Chart(
-        document.getElementById(canvasId).getContext('2d'),
-        {
-            type: "pie",
-            data: {
-                labels: strings[lang].chart[canvasId].labels,
-                datasets: [{
-                    data: [13, 32, 12, 31, 8],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)',
-                        'rgb(255, 205, 86)',
-                        'rgb(255, 205, 255)',
-                        'rgb(157,241,169)',
-                    ],
-                    hoverOffset: 4
-                }]
-
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'bottom',
-                    },
-                    title: {
-                        display: true,
-                        text: strings[lang].chart[canvasId].question,
-                        font: {
-                            size: 18,
-                        }
-                    }
-                }
-            },
-        }
-    )
+    pieChart(canvasId, strings[lang].chart[canvasId], [13, 32, 12, 31, 8]);
 }
 
 function chart7(canvasId, lang) {
-    new Chart(
-        document.getElementById(canvasId).getContext('2d'),
-        {
-            type: "pie",
-            data: {
-                labels: strings[lang].chart[canvasId].labels,
-                datasets: [{
-                    data: [89, 7],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)'
-                    ],
-                    hoverOffset: 4
-                }]
-
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'bottom',
-                    },
-                    title: {
-                        display: true,
-                        text: strings[lang].chart[canvasId].question,
-                        font: {
-                            size: 18,
-                        }
-                    }
-                }
-            },
-        }
-    )
+    pieChart(canvasId, strings[lang].chart[canvasId], [89, 7]);
 }
 
 function chart8(canvasId, lang) {
-    new Chart(
-        document.getElementById(canvasId).getContext('2d'),
-        {
-            type: "pie",
-            data: {
-                labels: strings[lang].chart[canvasId].labels,
-                datasets: [{
-                    data: [1, 4, 1, 1],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)',
-                        'rgb(255, 205, 86)',
-                        'rgb(157,241,169)',
-                    ],
-                    hoverOffset: 4
-                }]
-
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'bottom',
-                    },
-                    title: {
-                        display: true,
-                        text: strings[lang].chart[canvasId].question,
-                        font: {
-                            size: 18,
-                        }
-                    }
-                }
-            },
-        }
-    )
+    pieChart(canvasId, strings[lang].chart[canvasId], [2, 3, 1, 1]);
 }
 
 function chart9(canvasId, lang) {
-    new Chart(
-        document.getElementById(canvasId).getContext('2d'),
-        {
-            type: "pie",
-            data: {
-                labels: strings[lang].chart[canvasId].labels,
-                datasets: [{
-                    data: [2, 3, 1, 1],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)',
-                        'rgb(255, 205, 86)',
-                        'rgb(157,241,169)',
-                    ],
-                    hoverOffset: 4
-                }]
-
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'bottom',
-                    },
-                    title: {
-                        display: true,
-                        text: strings[lang].chart[canvasId].question,
-                        font: {
-                            size: 18,
-                        }
-                    }
-                }
-            },
-        }
-    )
+    pieChart(canvasId, strings[lang].chart[canvasId], [1, 4, 1, 1]);
 }
+
